c3c90fe5fdabe7fd331dc2955f17b9aece3a41bf
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-10-07 11:51:21 +0600

    Added preloader

--- frontend/src/containers/Form/Form.js
+++ frontend/src/containers/Form/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, IconButton, makeStyles, TextField} from "@material-ui/core";
+import {Backdrop, CircularProgress, Grid, IconButton, makeStyles, TextField} from "@material-ui/core";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import {useSelector, useDispatch} from "react-redux";
@@ -18,7 +18,9 @@ const useStyles = makeStyles(theme => ({
 const Form = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
+
     const message = useSelector(state => state.message);
+    const loading = useSelector(state => state.loading);
 
     const handleInputChange = e => {
         const {name, value} = e.target;
@@ -38,50 +40,56 @@ const Form = () => {
     };
 
     return (
-        <form className={classes.root}>
-            <Grid container direction="column" spacing={3}>
-                <Grid item>
-                    <TextField
-                        label="Decoded message"
-                        variant="outlined"
-                        multiline
-                        rows={5}
-                        fullWidth
-                        name="decoded"
-                        value={message.decoded}
-                        onChange={handleInputChange}
-                    />
+        loading
+            ?
+            <Backdrop open={loading}>
+                <CircularProgress color="inherit"/>
+            </Backdrop>
+            :
+            <form className={classes.root}>
+                <Grid container direction="column" spacing={3}>
+                    <Grid item>
+                        <TextField
+                            label="Decoded message"
+                            variant="outlined"
+                            multiline
+                            rows={5}
+                            fullWidth
+                            name="decoded"
+                            value={message.decoded}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid container item>
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            name="password"
+                            value={message.password}
+                            onChange={handleInputChange}
+                            className={classes.password}
+                        />
+                        <IconButton onClick={handleEncode}>
+                            <ArrowDownwardIcon/>
+                        </IconButton>
+                        <IconButton onClick={handleDecode}>
+                            <ArrowUpwardIcon/>
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            label="Encoded message"
+                            variant="outlined"
+                            multiline
+                            rows={5}
+                            fullWidth
+                            name="encoded"
+                            value={message.encoded}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid container item>
-                    <TextField
-                        label="Password"
-                        variant="outlined"
-                        name="password"
-                        value={message.password}
-                        onChange={handleInputChange}
-                        className={classes.password}
-                    />
-                    <IconButton onClick={handleEncode}>
-                        <ArrowDownwardIcon />
-                    </IconButton>
-                    <IconButton onClick={handleDecode}>
-                        <ArrowUpwardIcon />
-                    </IconButton>
-                </Grid>
-                <Grid item>
-                    <TextField
-                        label="Encoded message"
-                        variant="outlined"
-                        multiline
-                        rows={5}
-                        fullWidth
-                        name="encoded"
-                        value={message.encoded}
-                        onChange={handleInputChange}
-                    />
-                </Grid>
-            </Grid>
-        </form>
+            </form>
     );
 };
 
